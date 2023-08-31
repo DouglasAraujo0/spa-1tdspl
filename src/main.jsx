46b3d42355ca9f08f0d56e8f6a114ca8bf2261b2
DouglasAraujo0 <Rm552008@fiap.com.br>
2023-08-31 08:57:19 -0300

    Começando routers, CreateBrowseRouter, RouterProvider, etc...

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom';
+import EditarProdutos from "./router/EditarProduto.jsx"
+import Erro404 from "./router/Erro404.jsx"
+import Home from "./router/Home.jsx"
+import Produtos from "./router/Produtos.jsx"
+
+const router = createBrowserRouter([
+  { path: "/", element: <App/>,
+  children: [
+    { path: "/", element: <Home/> },
+    { path: "/produtos", element: <Produtos/> },
+    { path: "/editar/produtos/:id", element: <EditarProdutos/> },
+  ] 
+  }
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
