a7778975adea1cb4a96f1bcc3b2f8050af49e202
DouglasAraujo0 <Rm552008@fiap.com.br>
2023-09-14 12:12:17 -0300

    Excluir e editar produto concluido junto com estilização do botão de excluir e cancelar.

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -12,15 +12,6 @@ export default function lista() {
                 <li>
                     <Link to={"/produtos"}>Produtos</Link>
                 </li>
-                <li>
-                    <Link to={"/editar/produtos/1"}>Produto - 1</Link>
-                </li>
-                <li>
-                    <Link to={"/editar/produtos/2"}>Produto - 2</Link>
-                </li>
-                <li>
-                    <Link to={"/editar/produtos/3"}>Produto - 3</Link>
-                </li>
             </Header>
         </>
     );
