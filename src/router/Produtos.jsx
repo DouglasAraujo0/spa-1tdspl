238621765f066bdaaad22639cb5767d78c8f7110
rm552008 <rm552008@fiap.com.br>
2023-09-26 09:53:22 -0300

    botões criados junto com pasta e arquivo ModalInserir.

--- src/router/Produtos.jsx
+++ src/router/Produtos.jsx
@@ -1,9 +1,9 @@
-//import { ListaProdutos } from "../components/ListaProdutos"
 import { Link } from "react-router-dom"
 import classes from "./Produtos.module.css"
 import {AiFillEdit as Editar} from "react-icons/ai"
 import {RiDeleteBin2Fill as Excluir} from "react-icons/ri"
 import { useEffect, useState } from "react"
+import { ModalInserir} from "../components/ModalInserir/ModalInserir"
 
 export default function Produtos() {
     document.title = "Lista de Produtos: ";
@@ -25,13 +25,16 @@ export default function Produtos() {
     .catch(error => console.log(error))
 }, [])
     
+    const [open, setOpen] = useState(false);
+
 
     return (
         <div>
             <h1>Produtos de Informática - FIPAPI</h1>
             <p>Os Melhores Produtos do Mercado </p>
 
-        
+            {open ? <ModalInserir open = {open} setOpen = {setOpen}/> : ""}
+            <button onClick={() => setOpen(true)}>OPEN-MODAL</button>
 
             <table className={classes.estiloTabela}>
                 <thead className ={classes.tableHeaders}>
